975b6efd62d673e453b6ec7353ff6b85a9fafe2e
cSwormstedt <cswormstedt7@gmail.com>
2017-03-07 10:43:40 -0600

    modified

--- server/app.js
+++ server/app.js
@@ -4,6 +4,23 @@ var server  = require('http').createServer(app);
 var bodyParser = require('body-parser');
 var path	= require('path');
 var mongoose = require('mongoose');
+require('./db/db.js');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set(express.static(path.join(__dirname,'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+
+
+
+
+
+
+
+
 
 
 server.listen(3000, function(){
